3504c4962475243e95a2162dec02069d60468979
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-10-24 17:48:21 +0300

    Create 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module that defines Square class.
+"""
+
+
+class Square:
+    """
+    Class that defines Square objects.
+    """
+    def __init__(self, size=0):
+        """
+        Method that constructs a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Method that calculates Square area.
+
+        Return:
+            The area of the square.
+        """
+        return self.__size ** 2
