0cae5916d5f2a5f8b39a42ce762c9abaadd3aa84
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-10-31 21:22:21 +0200

    Solve task 5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module that has the definition of text_indentation function.
+"""
+
+
+def text_indentation(text):
+    """
+    Function that prints a text with 2 new lines after
+    each of these characters: { ., ?, : }
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    for delimiter in ".:?":
+        text = (delimiter + "\n\n").join(
+            [line.strip() for line in text.split(delimiter)])
+
+    print(text)
