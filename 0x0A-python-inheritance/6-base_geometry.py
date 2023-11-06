399f99b0001aab3577f60377fee83f107a91648e
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-11-06 20:19:17 +0200

    Create 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module that defines BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    Class that defines BaseGeometry.
+    """
+
+    def area(self):
+        """
+        Method that calculates area.
+        """
+
+        raise Exception("area() is not implemented")
