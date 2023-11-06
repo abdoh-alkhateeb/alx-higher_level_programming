1810d65ca772333b57384502eb9d438f480dd4cf
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-11-06 20:41:29 +0200

    Create 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module that defines Square class.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class that defines Square.
+    """
+
+    def __init__(self, size):
+        """
+        Method that constructs Square.
+        """
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area():
+        """
+        Method that calculates area.
+        """
+
+        return self.__size ** 2
