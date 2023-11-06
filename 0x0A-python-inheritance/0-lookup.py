0b73777b4aabcd6e1dfd6c08736e786164ac011d
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-11-06 19:50:22 +0200

    Create 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module that defines lookup function.
+"""
+
+
+def lookup(obj):
+    """
+    Function that returns list of available attributes and methods of object.
+    """
+    return dir(obj)
