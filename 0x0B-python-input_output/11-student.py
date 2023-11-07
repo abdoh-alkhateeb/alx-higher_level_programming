13f49b63f8cacc430681908a24a7c8b25fb6afe6
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-11-07 22:32:28 +0200

    Create 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Module that defines Student class.
+"""
+
+
+class Student:
+    """
+    Class that represents Student.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Constructor for Student class.
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Method that returns dict that contains info the object.
+        """
+
+        if attrs is None:
+            return self.__dict__
+
+        return {k: self.__dict__[k] for k in attrs if k in self.__dict__}
+
+    def reload_from_json(self, json):
+        """
+        Method that replaces all attributes of instance by using JSON dict.
+        """
+
+        for key in json:
+            self.__setattr__(key, json[key])
