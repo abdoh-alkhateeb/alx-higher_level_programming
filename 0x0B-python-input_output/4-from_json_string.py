2360662a717bea62e0bb416d2e369d5cc53bf354
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-11-07 21:44:07 +0200

    Create 4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module that defines from_json_string function.
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Function that returns an object represented by a JSON string.
+    """
+
+    return json.loads(my_str)
